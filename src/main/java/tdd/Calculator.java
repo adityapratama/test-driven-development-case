ddf4e604f46c6dff114b4c119ac8f3f34ab8eaad
aditya0010 <aditya.pratama.0010@gmail.com>
2014-09-09 09:24:58 +0700

    Importing the tdd project

---
+++ src/main/java/tdd/Calculator.java
@@ -0,0 +1,21 @@
+package tdd;
+
+public class Calculator {
+
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	public int minus(int i, int j) {
+		return i - j;
+	}
+
+	public int multi(int i, int j) {
+		return i * j;
+	}
+
+	public double devide(int i, int j) {
+		return i / j;
+	}
+
+}
---
+++ src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,36 @@
+package tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	private Calculator calculator;
+	
+	@Before
+	public void setUp(){
+		calculator = new Calculator();
+	}
+	@After
+	public void tearDown(){
+		
+	}
+	@Test
+	public void testAdd() {
+		assertEquals(5,(int)calculator.add(2,3));
+	}
+	@Test
+	public void testMinus() {
+		assertEquals(-1,(int)calculator.minus(2,3));
+	}
+	@Test
+	public void testMulti() {
+		assertEquals(6,(int)calculator.multi(2,3));
+	}
+	@Test
+	public void testDevide() {
+		assertTrue((5 == (double)calculator.devide(10,2)));
+	}
+}
